8ed03ec65c2548b6a2e057d8fdce99bcb316d8fa
東堤愛依璃 <higashitsutsumiaii@higashitsutsumiaiinoMacBook-Pro.local>
2025-07-17 15:45:33 +0900

    Roonviewに文字入れ

--- src/Rooms/Start/index.jsx
+++ src/Rooms/Start/index.jsx
@@ -5,19 +5,21 @@ import RoomView from '../../components/RoomView';
 
 
 
+
 export default function Start() {
   return (
-    <div >
-      <RoomView description={"何も無い部屋"} />
+    <div className="start-container">
+      <RoomView description={"何も無い部屋"} >
       
       <p className="room-text">中心の部屋。誰かの気配を感じる…</p>
       <p className="room-text">ドアが２つある。どっちへ行こうか？</p>
+      </RoomView>
    
       
-      <p className="button-group">
-      <Link to="/room1" className="horror-link">左</Link>
-      <Link to="/room2" className="horror-link">右</Link>
-      </p>
+      <div className="button-group">
+       <Link to="/room1" className="horror-link">左</Link>
+       <Link to="/room2" className="horror-link">右</Link>
+      </div>
      
     </div>
   );
